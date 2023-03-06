e7b6262cd71cef13ef4a2df90043e684eec12acc
Haraldur <haraldur@live.dk>
2023-03-06 09:07:34 +0100

    DataType

---
+++ DataType/Datatype/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Datatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(sizeof(int));
+        }
+    }
+}
