7f50920c88c11131eedeb0f22098c0d23e7316f2
Haraldur <haraldur@live.dk>
2023-03-06 11:01:59 +0100

    New Diagram

--- DataType/Datatype/Program.cs
+++ DataType/Datatype/Program.cs
@@ -19,32 +19,65 @@ namespace Datatype
 
             Console.WriteLine("Value compined: " + newInt);
             Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("-----------------INT DIAGRAM-----------------");
-            UInt64 startCount = 1000;
-            Console.Write("     ");
-            for (int i = 0; i < 10; i++)
+            Console.WriteLine("---------------------CHAR--------------------");
+            Console.WriteLine("Char Size: " + sizeof(char));
+
+            Console.WriteLine("MinValue: " + char.MinValue);
+            Console.WriteLine("MacValue: " + char.MaxValue);
+
+            int charValue = char.MaxValue;
+            UInt64 convertedChar = Convert.ToUInt64(charValue);
+            //Add 1 because of 0
+            UInt64 newChar = convertedChar + convertedChar + 1;
+
+            Console.WriteLine("Value compined: " + newChar);
+            Console.WriteLine("--------------------------------------------");
+
+            int intSize = sizeof(int);
+            int charSize = sizeof(char);
+            int floatSize = sizeof(float);
+
+            Console.WriteLine("--------------DIAGRAM OF BYTE---------------");
+            Console.WriteLine("         1 2 3 4 5 6 7 8 9 10 11 12 13 14 15");
+            Console.Write("Int      ");
+            for (int i = 0; i < intSize; i++)
             {
-                Console.Write(startCount);
-                Console.Write("     ");
-                startCount = startCount * 5;
+                Console.Write('*');
+                Console.Write(' ');
             }
             Console.WriteLine("");
-            Console.Write("Int  ");
-            string calcNewInt = Convert.ToString(newInt);
-            for (int i = 0; i < 10; i++)
-            {
-                string tests = Convert.ToString(newInt);
 
-                for (int ind = 0; ind < tests.Length; ind++)
-                {
-                    Console.Write("*");
-                }
-                Console.Write("     ");
-                newInt = newInt / 10;
+            Console.Write("Char     ");
+            for (int i = 0; i < charSize; i++)
+            {
+                Console.Write('*');
+                Console.Write(' ');
             }
             Console.WriteLine("");
 
-            Console.WriteLine("---------------------------------------------");
+            Console.Write("Float    ");
+            for (int i = 0; i < floatSize; i++)
+            {
+                Console.Write('*');
+                Console.Write(' ');
+            }
+            Console.WriteLine("");
+
+            //OLD
+            //Console.Write("Char ");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    string charLength = Convert.ToString(newChar);
+
+            //    for (int ind = 0; ind < charLength.Length; ind++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    newChar = newChar / 5;
+            //}
+            //Console.WriteLine("");
+
+            Console.WriteLine("--------------------------------------------");
 
         }
     }
