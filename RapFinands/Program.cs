3f183a0930a4dc1376563652235cba420564edcf
Haraldur <haraldur@live.dk>
2023-03-13 13:19:45 +0100

    BugFix 3
    Changed variable name in function GemTrans it was using a referring to wrong variable

--- RapFinands/Program.cs
+++ RapFinands/Program.cs
@@ -15,7 +15,6 @@ namespace Rap_Finands
         public static string reginummer = "4242";
         public static string datafil = "bank.json"; //Her ligger alt data i
         public static List<Konto> konti;
-        public static float belob;
         static void Main(string[] args)
         {
             Console.WriteLine("Henter alt kontodata");
@@ -152,7 +151,7 @@ namespace Rap_Finands
             if (saldo + beløb < 0) return false;
             var t = new Transaktion();
             t.tekst = tekst;
-            t.amount = belob;
+            t.amount = beløb;
             t.saldo = t.amount + saldo;
             t.dato = DateTime.Now;
             
