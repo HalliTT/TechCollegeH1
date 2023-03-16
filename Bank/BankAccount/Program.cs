826b68ae244886bc8a7e17b6954af366e765ce1a
Haraldur <haraldur@live.dk>
2023-03-16 09:49:56 +0100

    basic setup

---
+++ Bank/BankAccount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Account
+    {
+        float account;
+    }
+
+    class Bank
+    {
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
